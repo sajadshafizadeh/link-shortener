9ab102f0f5b5ffc240fc88c98b01b0bc2f98ba28
Sajad Shafizadeh <vip.saj.71@gmail.com>
2021-07-26 19:46:16 +0430

    Rotes file added as a external configuration file - incompelet

--- src/app.php
+++ src/app.php
@@ -1,9 +1,17 @@
 <?php
 
 use Symfony\Component\Routing;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+
 
 $routes = new Routing\RouteCollection();
 $routes->add('hello', new Routing\Route('/hello/{name}', ['name' => 'World']));
 $routes->add('bye', new Routing\Route('/bye'));
+
+
+$fileLocator = new FileLocator([__DIR__]);
+$loader = new YamlFileLoader($fileLocator);
+$routes = $loader->load('routes.yaml');
 
 return $routes;
