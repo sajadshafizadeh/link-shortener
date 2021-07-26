30888b5b4aaa19795c648cad99632b451e6c573b
Sajad Shafizadeh <vip.saj.71@gmail.com>
2021-07-26 22:07:58 +0430

    Simfony yaml package added

--- src/app.php
+++ src/app.php
@@ -3,7 +3,7 @@
 use Symfony\Component\Routing;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Routing\Loader\YamlFileLoader;
-
+use Symfony\Component\Yaml\Parser as YamlParser;
 
 $routes = new Routing\RouteCollection();
 $routes->add('hello', new Routing\Route('/hello/{name}', ['name' => 'World']));
@@ -12,6 +12,6 @@ $routes->add('bye', new Routing\Route('/bye'));
 
 $fileLocator = new FileLocator([__DIR__]);
 $loader = new YamlFileLoader($fileLocator);
-$routes .= $loader->load('routes.yaml');
+$routes = $loader->load('routes.yaml');
 
 return $routes;
