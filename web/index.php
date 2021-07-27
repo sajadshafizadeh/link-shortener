317d9b6d513975d687624bde084fcf73966eb18c
Sajad Shafizadeh <vip.saj.71@gmail.com>
2021-07-28 00:55:52 +0430

    Clean up

--- web/index.php
+++ web/index.php
@@ -5,6 +5,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
@@ -17,16 +18,17 @@ try {
 
     extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
     ob_start();
-    $path = sprintf(__DIR__.'/../src/pages/%s.php', $filename);
 
+    $path = sprintf(__DIR__.'/../src/pages/%s.php', $filename);
+    
     if (file_exists($path)){
     	include $path;
     } else {
-    	throw new Routing\Exception\ResourceNotFoundException();
+    	throw new ResourceNotFoundException();
     }
 
     $response = new Response(ob_get_clean());
-} catch (Routing\Exception\ResourceNotFoundException $exception) {
+} catch (ResourceNotFoundException $exception) {
     $response = new Response('Not Found', 404);
 } catch (Exception $exception) {
     $response = new Response('An error occurred', 500);
