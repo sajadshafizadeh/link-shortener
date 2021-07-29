b66807d6c1462c7840e3f56da5ee14cf11c133a4
Sajad Shafizadeh <vip.saj.71@gmail.com>
2021-07-30 00:02:35 +0430

    Twig added

--- src/Application/Controller/hello.php
+++ src/Application/Controller/hello.php
@@ -4,11 +4,13 @@ namespace Application\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use \Twig\Environment;
 
 class Hello{
 
-    public function index(Request $request): Response {
+    public function index(Request $request, Environment $twig): Response {
+
 		return $response = new Response("Hello " . htmlspecialchars($request->get('name'), ENT_QUOTES, 'UTF-8'));
     }
-
+    
 }
--- web/index.php
+++ web/index.php
@@ -6,6 +6,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use \Twig\Loader\FilesystemLoader;
+use \Twig\Environment;
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
@@ -23,17 +25,21 @@ try {
         $request->attributes->set($key, $value);
     }
 
+    // Init Twig
+    $loader = new FilesystemLoader(__DIR__.'/../src/Application/Template');
+    $twig = new Environment($loader, []);
+
     // To make an object of the controller class
     $controller_name = $request->get('controller');
     $controller = new $controller_name;
 
     // To call the specified method of the controller class in the route
-    $response = $controller->{$request->get('action')}($request);
+    $response = $controller->{$request->get('action')}($request, $twig);
 
 } catch (ResourceNotFoundException $exception) {
     $response = new Response('Not Found', 404);
 } catch (Throwable $exception) {
-    $response = new Response('An error occurred', 500);
+    $response = new Response($exception->getMessage(), 500);
 }
 
 $response->send();
